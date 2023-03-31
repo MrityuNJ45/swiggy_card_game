9135153c57601fb3b31b7bf20c3c967e0666c0a6
Mrityunjay Sarkar <mrityunjaysorcar@gmail.com>
2023-03-31 13:51:38 +0530

    added structure of playgame class

---
+++ card_Game/src/main/java/org/example/entity/GamePlay.java
@@ -0,0 +1,23 @@
+package org.example.entity;
+
+import java.util.ArrayList;
+
+public class GamePlay {
+
+    private ArrayList<Card> deck;
+
+    private ArrayList<Player> players;
+
+    private ArrayList<Card> drawPile;
+
+    private ArrayList<Card> discardPile;
+
+
+    public void playGame(){
+
+    }
+
+
+
+
+}
