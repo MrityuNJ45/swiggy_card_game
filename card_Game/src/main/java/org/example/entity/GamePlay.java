103bedacf4ced76c4eef2559219e31d22435a05f
Mrityunjay Sarkar <mrityunjaysorcar@gmail.com>
2023-03-31 14:04:18 +0530

    added playgame method in GamePlay class and starting to build logic

--- card_Game/src/main/java/org/example/entity/GamePlay.java
+++ card_Game/src/main/java/org/example/entity/GamePlay.java
@@ -1,6 +1,7 @@
 package org.example.entity;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class GamePlay {
 
@@ -13,7 +14,40 @@ public class GamePlay {
     private ArrayList<Card> discardPile;
 
 
-    public void playGame(){
+    public void playGame() throws Exception{
+
+        Scanner sc = new Scanner(System.in);
+        int numOfPlayers = sc.nextInt();
+
+        if (numOfPlayers < 2 || numOfPlayers > 4) {
+            throw new Exception("Invalid number of players ... Retry again....");
+        }
+
+        deck = new Deck().getDeck();
+
+        players = new ArrayList<>();
+        for (int i = 1; i <= numOfPlayers; i++) {
+            Player p = new Player(i);
+
+            // adding 5 - 5 cards in each player
+            for (int j = 1; j <= 5; j++) {
+                p.addCard(deck.get(deck.size() - 1));
+                deck.remove(deck.size() - 1);
+            }
+            players.add(p);
+            //  System.out.println(p);
+        }
+
+        discardPile = new ArrayList<>();
+        discardPile.add(deck.get(0));
+        deck.remove(0);
+
+        drawPile = new ArrayList<>();
+        for (Card c : deck) {
+            drawPile.add(c);
+        }
+
+
 
     }
 
